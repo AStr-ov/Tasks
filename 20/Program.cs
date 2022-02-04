e394ab8d15f4097647d97eccbbffa9fce80bc0c6
Astr <boxreg2000@yandex.ru>
2022-02-04 21:11:16 +0300

    Задача №20

---
+++ 20/Program.cs
@@ -0,0 +1,21 @@
+﻿//20. Задать номер четверти, показать диапазоны для возможных координат
+Console.WriteLine("Введите номер интересуемой четверти");
+int a=int.Parse(Console.ReadLine());
+string b="бесконечность";
+string c="Диапазон возможных координат";
+if (a==1)
+{
+    Console.WriteLine($"{c} в {a} четверти X=[0;+{b}], Y=[0;+{b}]");
+}
+if (a==2)
+{
+    Console.WriteLine($"{c} вo {a} четверти X=]-{b};0], Y=[0;+{b}]");
+}
+if (a==3)
+{
+    Console.WriteLine($"{c} в {a} четверти X=]-{b};0], Y=]-{b};0]");
+}
+if (a==4)
+{
+    Console.WriteLine($"{c} в {a} четверти X=[0;+{b}], Y=]-{b};0]");
+}

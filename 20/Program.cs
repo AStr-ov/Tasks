c3fbfba987b6aa605ea6bbf88251fcfd1c1e8015
Astr <boxreg2000@yandex.ru>
2022-02-07 22:28:10 +0300

    №20 исключил 0

--- 20/Program.cs
+++ 20/Program.cs
@@ -3,8 +3,8 @@ Console.WriteLine("Введите номер интересующей четве
 int a=int.Parse(Console.ReadLine());
 string b="бесконечность";
 string c="Диапазон возможных координат";
-if (a==1) Console.WriteLine($"{c} в {a} четверти X=[0;+{b}[, Y=[0;+{b}[");
-else if (a==2) Console.WriteLine($"{c} вo {a} четверти X=]-{b};0], Y=[0;+{b}[");
-else if (a==3) Console.WriteLine($"{c} в {a} четверти X=]-{b};0], Y=]-{b};0]");
+if (a==1) Console.WriteLine($"{c} в {a} четверти X=]0;+{b}[, Y=]0;+{b}[");
+else if (a==2) Console.WriteLine($"{c} вo {a} четверти X=]-{b};0[, Y=]0;+{b}[");
+else if (a==3) Console.WriteLine($"{c} в {a} четверти X=]-{b};0[, Y=]-{b};0[");
 else
-Console.WriteLine($"{c} в {a} четверти X=[0;+{b}[, Y=]-{b};0]");
+Console.WriteLine($"{c} в {a} четверти X=]0;+{b}[, Y=]-{b};0[");

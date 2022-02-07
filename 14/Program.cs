a9af9281fce59359afc49cbf8440036eda44a6fa
Astr <boxreg2000@yandex.ru>
2022-02-07 14:32:02 +0300

    №14 для любых чисел

--- 14/Program.cs
+++ 14/Program.cs
@@ -1,10 +1,24 @@
 ﻿//14. Найти третью цифру числа или сообщить, что её нет
-int a=new Random().Next(1, 199);
-if (a/100>0)
+int a=new Random().Next(0, 9999999);
+int b=a;
+int c=a;
+int count=0;
+while (b>0)
 {
-    Console.WriteLine($"Третья цифра числа {a} : {a%10}");
+    b=a/10;
+    a=b;
+    count++;
 }
-else
+a=c;
+if (count>3)
 {
-    Console.WriteLine($"Число {a} не имеет третьей цифры");
+    while (count>3)
+    {
+        c=c/10;
+        count--;
+    }
 }
+if (count>=3) Console.WriteLine($"третьей цифрой числа {a} является {c%10}");
+else Console.WriteLine($"третьей цифры в числе {a} нет");
+    
+

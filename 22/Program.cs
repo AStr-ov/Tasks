e8492cf4ff8d808cca04298e7bbe06e59fd2af14
Astr <boxreg2000@yandex.ru>
2022-02-08 11:06:44 +0300

    Задача №22 для плоскости

---
+++ 22/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 22: Найти расстояние между точками в пространстве 2D/3D
+Console.WriteLine("Введите координату Х точки А");
+int ax=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату Y точки А");
+int ay=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату Х точки B");
+int bx=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату Y точки B");
+int by=int.Parse(Console.ReadLine());
+double result=Math.Sqrt((bx-ax)*(bx-ax)+(by-ay)*(by-ay));
+Console.WriteLine($"Расстояние от точки А({ax};{ay}) до точки В({bx};{by}) = {result}");

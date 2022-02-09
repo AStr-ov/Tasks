ec462defdfe15bdcdb0831a2a8a20fe3d836162b
AStr-ov <97983390+AStr-ov@users.noreply.github.com>
2022-02-09 11:16:18 +0300

    Update Program.cs

--- 14/Program.cs
+++ 14/Program.cs
@@ -2,10 +2,12 @@
 int a=new Random().Next(0, 9999999);
 int b=a;
 int c=a;
-for (int count = 0; b>a; count++)
+int count=0;
+while (b>0)
 {
     b=a/10;
     a=b;
+    count++;
 }
 a=c;
 if (count>3)
@@ -18,5 +20,4 @@ if (count>3)
 }
 if (count>=3) Console.WriteLine($"третьей цифрой числа {a} является {c%10}");
 else Console.WriteLine($"третьей цифры в числе {a} нет");
-    
-
+   

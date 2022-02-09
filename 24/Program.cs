48170b3b95b26134274986caa5323f4ca6d10a3d
Astr <boxreg2000@yandex.ru>
2022-02-09 09:00:44 +0300

    Задача №24

---
+++ 24/Program.cs
@@ -0,0 +1,8 @@
+﻿//24. Найти кубы чисел от 1 до N
+Console.WriteLine("Введите число границы таблицы");
+int N=int.Parse(Console.ReadLine());
+Console.WriteLine($"Число  Куб числа");
+for (int i = 1; i <= N; i++)
+{
+        Console.WriteLine($" {i}      {i*i*i}");
+}

2dd85894b5c4f0aff6e19b05a60327e67f485574
Astr <boxreg2000@yandex.ru>
2022-02-12 15:40:42 +0300

    Задача №28

---
+++ 28/Program.cs
@@ -0,0 +1,10 @@
+﻿//28. Подсчитать сумму цифр в числе
+int number=new Random().Next(1, 999999);
+int n=number;
+int result=0;
+for (int i = 0; 1 <= number; i++)
+{
+    result=result+number%10;
+    number=number/10;
+}
+Console.WriteLine($"Сумма цифр в числе {n}  {result}");

ef98ecab62216f262a464a68cc55b617aa731e11
Astr <boxreg2000@yandex.ru>
2022-02-12 21:23:41 +0300

    Задача №30

---
+++ 30/Program.cs
@@ -0,0 +1,14 @@
+﻿//30. Показать кубы чисел, заканчивающихся на четную цифру
+Console.WriteLine($"Четное число    Куб числа");
+int a=1;
+int n=100;
+double res=1;
+for (int i = 0; i <=n; i++)
+{
+    res=a*a*a;
+    if (a%2==0)
+    {
+        Console.WriteLine($"{a}               {res}");
+    }
+    a++;
+}

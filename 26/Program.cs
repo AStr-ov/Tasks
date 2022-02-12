41021f91b2e11f8fd37a6d79ebb7b5e306e9410b
Astr <boxreg2000@yandex.ru>
2022-02-12 14:55:50 +0300

    Задача №26

---
+++ 26/Program.cs
@@ -0,0 +1,9 @@
+﻿//26. Возведите число А в натуральную степень B используя цикл
+int A=new Random().Next(1,100);
+int B=new Random().Next(0,20);
+double res=1;
+for (int i = 1; i <= B; i++)
+{
+    res=res*A;
+}
+Console.WriteLine($"Число {A} в степени {B} ={res}");

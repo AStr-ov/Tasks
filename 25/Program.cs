8170cee9a90683e9994907ef9b63f11ff8a8c29f
Astr <boxreg2000@yandex.ru>
2022-02-12 14:19:25 +0300

    Задача №25

---
+++ 25/Program.cs
@@ -0,0 +1,10 @@
+﻿//25. Найти сумму чисел от 1 до А
+Console.WriteLine("Введите граничную цифру");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"Сума чисел от 1 до {A} ={A*(A+1)/2}");
+//int n = 0;   или по дурацки
+//for (int i = 0; i <= A; i++)
+//{
+   // Console.WriteLine(n+=i);
+//}

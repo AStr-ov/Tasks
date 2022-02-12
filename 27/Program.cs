6008731d41eaf96553a385e73a88afadb852861a
Astr <boxreg2000@yandex.ru>
2022-02-12 15:23:54 +0300

    Задача №27

---
+++ 27/Program.cs
@@ -0,0 +1,9 @@
+﻿//27. Определить количество цифр в числе
+int num = new Random().Next(1, 1000000);
+Console.WriteLine(num);
+int i = 0;
+for (i = 0; 1 <= num; i++)
+{
+    num = num / 10;
+}
+Console.WriteLine($" {i} цифр(ы)");

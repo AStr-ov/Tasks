32001e1984798f5bae94551b421ba69c52a990da
Astr <boxreg2000@yandex.ru>
2022-02-14 21:20:23 +0300

    Задача №40

---
+++ 40/Program.cs
@@ -0,0 +1,22 @@
+﻿//40. В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+int[] arr = new int[10];
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(-100, 100);
+    Console.Write(arr[i] + " ");
+}
+int max = arr[0];
+int min = arr[0];
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+    if (arr[i] < min)
+    {
+        min = arr[i];
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Разница между максимальным элементом массива ({max}) и его минимальным элементом ({min}) составляет  {max-min}");

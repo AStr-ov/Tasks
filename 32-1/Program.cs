3e69abf8981f437af4af82c144aadab0c8f50cb9
Astr <boxreg2000@yandex.ru>
2022-02-14 17:09:39 +0300

    Задача №32 через функции

---
+++ 32-1/Program.cs
@@ -0,0 +1,20 @@
+﻿//32. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+Console.WriteLine("Масив");
+void fill(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+    }
+}
+void print(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i]+" ");
+    }
+}
+int[] array = new int[8];
+fill(array);
+print(array);
+Console.WriteLine();

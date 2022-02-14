bbfe2ef48a9383ccf3810d80e2bac8835c50d701
Astr <boxreg2000@yandex.ru>
2022-02-14 20:41:20 +0300

    Задача №39

---
+++ 39/Program.cs
@@ -0,0 +1,16 @@
+﻿//39. Найти произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+int[] mat=new int[7];
+int result=1;
+for (int i = 0; i < mat.Length; i++)
+{
+    mat[i]=new Random().Next(-10,10);
+    Console.Write(mat[i]+";");
+}
+Console.WriteLine();
+for (int i = 0; i < (mat.Length+1)/2; i++)
+{
+    result=mat[i]*mat[mat.Length-1-i];
+    Console.Write(result+" ");
+}
+Console.WriteLine();

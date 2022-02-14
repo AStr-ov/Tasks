f4a3ec522205d119732767bb2ac2284cc803baff
Astr <boxreg2000@yandex.ru>
2022-02-14 17:41:03 +0300

    Задачв №35

---
+++ 35/Program.cs
@@ -0,0 +1,15 @@
+﻿//35. Определить, присутствует ли в заданном массиве, некоторое число
+int[] mas = { -5, 0, 11, 8, -4 };
+bool result = false;
+Console.WriteLine("Введите искомое число");
+int x = int.Parse(Console.ReadLine());
+for (int i = 0; i < mas.Length; i++)
+{
+    //Console.Write($"{mas[i]};");
+    if (mas[i] == x) result=true;
+}
+Console.WriteLine();
+if (result)
+Console.WriteLine("Искомое число  присутствует в заданном массиве");
+else
+Console.WriteLine("Искомого числа   нет в заданном массиве");

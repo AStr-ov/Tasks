e8d680b17166a3b618f0988241ef04cce38fa172
Astr <boxreg2000@yandex.ru>
2022-02-16 22:29:22 +0300

    Задача №45 через рекурсию

---
+++ 45-1/Program.cs
@@ -0,0 +1,14 @@
+﻿//45. Показать числа Фибоначчи
+Console.WriteLine("Введите количество показываемых чисел");
+int n = int.Parse(Console.ReadLine());
+int Fibonacci(int num)
+{
+    if (num == 1 || num == 2)
+        return 1;
+    else
+        return Fibonacci(num - 1) + Fibonacci(num - 2);
+}
+for (int i = 1; n>=i; i++)
+{
+    Console.WriteLine(Fibonacci(i));
+}

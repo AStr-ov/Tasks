de5a7bc6d1ba4d6ea775ba9d6ce57796ab3f0660
Astr <boxreg2000@yandex.ru>
2022-02-16 19:01:22 +0300

    Задача №45 через for

---
+++ 45/Program.cs
@@ -0,0 +1,11 @@
+﻿//45. Показать числа Фибоначчи
+int a = 1;
+int b = 1;
+int count = 10;
+for (int i = 0; i <= count; i++)
+{
+    Console.Write($"{a},{b},");
+    a = a + b;
+    b = b + a;
+}
+Console.WriteLine();

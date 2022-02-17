034d537c1ce28ffb4a8d348b1f1d0c917527555f
Astr <boxreg2000@yandex.ru>
2022-02-17 16:37:07 +0300

    Задача №41

---
+++ 41/Program.cs
@@ -0,0 +1,13 @@
+﻿//41. Выяснить являются ли три числа сторонами треугольника
+Console.WriteLine("Введите длину первой стороны");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите длину второй стороны");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите длину третьей стороны");
+int c = int.Parse(Console.ReadLine());
+if (a < b + c && b < a + c && c < a + b)
+{
+    Console.WriteLine($"Числа {a}, {b}, {c} могут составлять стороны треугольника");
+}
+else
+    Console.WriteLine($"Числа {a}, {b}, {c} не могут составлять стороны треугольника");

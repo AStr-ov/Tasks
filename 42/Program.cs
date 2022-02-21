ffc65fced616c972e10a7a965793f9bce96e9f1e
Astr <boxreg2000@yandex.ru>
2022-02-21 22:03:18 +0300

    Задача №42 через Split

---
+++ 42/Program.cs
@@ -0,0 +1,10 @@
+﻿//42. Определить сколько чисел больше 0 введено с клавиатуры
+Console.WriteLine("Введите через пробел числа");
+var a=Console.ReadLine().Split(" ").ToArray();
+int count=0;
+for (int i = 0; i < a.Length; i++)
+{
+        int number=int.Parse(a[i]);
+        if (number>0) count++;
+}
+Console.WriteLine($"Введено {count} чисел(a) больше нуля");

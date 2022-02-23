7c865a06b81a959dbed862b03b96c1d26f1f1d1f
Astr <boxreg2000@yandex.ru>
2022-02-23 09:55:19 +0300

    Задача №46 через сплит и селект

---
+++ 46-1/Program.cs
@@ -0,0 +1,14 @@
+﻿//46. Написать программу масштабирования фигуры
+Console.WriteLine("Введите координаты точек");
+var data = Console.ReadLine()
+                  .Replace("(", "")
+                  .Replace(")", "")
+                  .Replace(",", " ")
+                  .Split(" ")
+                  .Select(double.Parse)
+                  .ToArray();
+Console.WriteLine("Введите коэффициент масшатабирования");
+double k = double.Parse(Console.ReadLine());
+Console.WriteLine("Координаты точек в результате масштабирования с коэффицентом" + " " + k);
+for (int i = 0; i < data.Length; i = i + 2)
+    Console.WriteLine($"({data[i] * k} , {data[i + 1] * k})");

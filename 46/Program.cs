5801bf8b5a087dda8d3a0c1aac480af1eb47bf97
Astr <boxreg2000@yandex.ru>
2022-02-23 09:48:40 +0300

    Задача №46 через кортеж

---
+++ 46/Program.cs
@@ -0,0 +1,15 @@
+﻿//46. Написать программу масштабирования фигуры
+Console.WriteLine("Введите через пробел координаты точек в формате -  x1,y1 x2,y2");
+string coordinates = Console.ReadLine();
+Console.WriteLine("Ведите коэффициент масштабирования");
+double k = double.Parse(Console.ReadLine());
+Console.WriteLine($"Координаты точек в результате массштабирования с коэффициентом {k}:");
+var data = coordinates
+    .Split(' ')
+    .Select(point => point.Split(','))
+    .Select(c => (x: int.Parse(c[0]), y: int.Parse(c[1])))
+    .ToArray();
+for (int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine($"({data[i].x * k};{data[i].y * k})");
+}

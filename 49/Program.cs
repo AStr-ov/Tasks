58a2490542a1a85eb24a04e1a9196280035ea977
Astr <boxreg2000@yandex.ru>
2022-02-23 17:46:45 +0300

    Задача №49

---
+++ 49/Program.cs
@@ -0,0 +1,15 @@
+﻿//49. Показать двумерный массив размером m×n заполненный вещественными числами
+Console.WriteLine("Введите количество строк массива");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива");
+int n = int.Parse(Console.ReadLine());
+double[,] array = new double[m, n];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().NextDouble()*200-100;
+        Console.Write(array[i, j] + ";");
+    }
+    Console.WriteLine();
+}

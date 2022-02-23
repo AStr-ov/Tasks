d1416322358567d543dac65c6e89c4b24a7d4194
Astr <boxreg2000@yandex.ru>
2022-02-23 18:43:05 +0300

    Задача №51

---
+++ 51/Program.cs
@@ -0,0 +1,12 @@
+﻿//51. Задать двумерный массив следующим правилом: Aₘₙ = m+n
+int a=5;
+int b=8;
+int[,] mass=new int[a,b];
+for (int m = 0; m < mass.GetLength(0); m++)
+{
+    for (int n = 0; n < mass.GetLength(1); n++)
+    {
+        Console.Write($"{mass[m,n]=m+n} ");
+    }
+    Console.WriteLine();
+}

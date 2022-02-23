ead2a8d6ac6804cf363a56497838e9d6fe59bb3d
Astr <boxreg2000@yandex.ru>
2022-02-23 17:23:54 +0300

    Задача №47

---
+++ 47/Program.cs
@@ -0,0 +1,25 @@
+﻿//47. Написать программу копирования массива
+int[] mas=new int[10];
+int[] arr=new int[mas.Length];
+void FillArr(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]=new Random().Next(0,100);
+    }
+}
+void PrintArr(int[] array)
+{
+    for (int i = 0; i <array.Length; i++)
+    {
+        Console.Write(array[i]+";");
+    }
+}
+FillArr(mas);
+PrintArr(mas);
+Console.WriteLine();
+for (int i = 0; i < mas.Length; i++)
+{
+    arr[i]=mas[i];
+}
+PrintArr(arr);

c36ccbd5d537867cfac6405673566066c553622a
Astr <boxreg2000@yandex.ru>
2022-03-01 22:04:32 +0300

    Задача № 63 наполнен по порядку

---
+++ 63/Program.cs
@@ -0,0 +1,27 @@
+﻿//63. Сформировать трехмерный массив не повторяющимися
+// двузначными числами показать его построчно на экран 
+//выводя индексы соответствующего элемента
+int a = 4;
+int b = 5;
+int c = 4;
+if (a * b * c < 90)
+{
+    int[,,] array = new int[a, b, c];
+    int q = 10;
+    for (int i = 0; i < array.GetLength(0); i++) 
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = q;
+                q++;
+                Console.WriteLine($"Элемент с индексом ({i},{j},{k} : {array[i, j, k]})");
+            }
+            
+        }
+    }
+}
+else
+    Console.WriteLine("Не хватит уникальных двузнычных чисел для заполнения матрицы данного размера");
+    

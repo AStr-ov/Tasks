241b63f21db831a3278ce6e2bc58ebad3885eaeb
Astr <boxreg2000@yandex.ru>
2022-03-04 20:42:29 +0300

    Задача №67

---
+++ 67/Program.cs
@@ -0,0 +1,11 @@
+﻿//67. Показать натуральные числа от N до 1, N задано
+
+void Metod(int n)
+{
+    Console.Write(n+";");
+    if (n==1)return ;
+    Metod(n-1);
+}
+Console.WriteLine("Введите чмсло");
+int N=int.Parse(Console.ReadLine());
+Metod(N);
